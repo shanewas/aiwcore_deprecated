7e5da2a053aa91a4fb206a76fc3bdc630c1ba3e9
shanewas ahmed nabil <shanewasahmed@gmail.com>
2020-01-13 11:59:28 +0600

    hierarchy structured ||  moduled

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const electron = require('electron')
 const url = require('url')
 const path = require('path')
-const windowCollection = require('./js/window_collection')
+const menu = require('./js/menu')
+
 // import {addwindow, createAddWindow} from 'window_collection.js';
 
 const { app, BrowserWindow, Menu } = electron
-
 let mainWindow
 // let addWindow
 
@@ -24,44 +24,20 @@ app.on('ready', function(){
     mainWindow.on('closed', () => app.quit())
 
     //Build menu
-    const mainMenu = Menu.buildFromTemplate(mainMenuTempate)
+    const mainMenu = Menu.buildFromTemplate(menu.mainMenuTempate)
     //Inset menu
     Menu.setApplicationMenu(mainMenu)
 })
 
 
-const mainMenuTempate = [
-    {
-        label: 'Home',
-        submenu: [
-            {
-                label: 'Web Scrapping',
-                click(){
-                    windowCollection.createAddWindow()
-                }
-            },
-            {
-                label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click(){
-                    app.quit()
-                }
-            }
-        ]
-    },
-    {
-        label: 'Help'
-    }
-]
-
 //if mac add empty onject with menu
 if (process.platform == 'darwin'){
-    mainMenuTempate.unshift({})
+    menu.mainMenuTempate.unshift({})
 }
 
 //Add developer tools inf not in prod
 if(process.env.NODE_ENV !== 'production'){
-    mainMenuTempate.push({
+    menu.mainMenuTempate.push({
         label: 'Developer Tools',
         submenu: [
             {
